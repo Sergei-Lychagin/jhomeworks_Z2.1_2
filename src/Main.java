d09583fbf78df11237d1d123acd65cb065ca5966
Sergei Lychagin <lychagin0@gmail.com>
2021-03-11 13:52:25 +0400

    форматирование

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-float deposit = 300.90f;
-float contribution = 1200.50f;
-float balance = deposit + contribution;
+        float deposit = 300.90f;
+        float contribution = 1200.50f;
+        float balance = deposit + contribution;
 
-if (balance > 1000) {
-    
-int bonus = (int)(balance/100);
-    balance = balance + bonus;
-    System.out.println("Your bonus" + "   " + bonus + "rub");
-    
-    }
-else {balance = balance;}
+        if (balance > 1000) {
 
-System.out.println("Your balance is" + "  " + balance + "rub");
-        
+            int bonus = (int) (balance / 100);
+            balance = balance + bonus;
+            System.out.println("Your bonus" + "   " + bonus + "rub");
+
+        } else {
+            balance = balance;
+        }
+
+        System.out.println("Your balance is" + "  " + balance + "rub");
+
     }
 }
